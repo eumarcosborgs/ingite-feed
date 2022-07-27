34d68955d64efedda4ad97e69b21eced138e3e99
Nome Sobrenome <eumarcosborgs@gmail.com>
2022-07-27 01:03:16 -0300

    create post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import './global.css';
-
 import { Header } from "./components/Header" 
 import { Sidebar } from "./components/Sidebar";
-
-import { Post } from "./Post"
+import { Post } from "./components/Post"
 
 import styles from './App.module.css';
+
+import './global.css';
 
 function App() {
   return (
